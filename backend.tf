28a05cfc07930de607dd23871a2ec85e263daf23
Akash Arjun Dalvi <157683004+akash-dalvi22@users.noreply.github.com>
2025-12-27 11:20:32 +0530

    Update backend.tf
    updated backend

--- backend.tf
+++ backend.tf
@@ -2,7 +2,8 @@ terraform {
   backend "s3" {
     bucket = "nextpath-app-project-terraform-state"
     key = "main"
-    region = "us-east-1"
+    region = "ap-south-1"
     dynamodb_table = "my-dynamodb-table"
   }
 }
+
